e1642a2f391d8cfbdfe312aa19666d67f90398fa
Rohkyoungjin <rudwls468@gmail.com>
2020-08-27 17:49:19 +0900

    Update LoginScreen.js

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -3,17 +3,60 @@ import {
   View,
   Text,
   StyleSheet,
-  TextInput,
-  TouchableOpacity,
   Image,
   Dimensions,
+  TextInput,
 } from 'react-native';
 import * as firebase from 'firebase';
-import Animated from 'react-native-reanimated';
-import {TopGestureHandler} from 'react-native-gesture-handler';
-
+import Animated, {Easing} from 'react-native-reanimated';
+import {TapGestureHandler, State} from 'react-native-gesture-handler';
 const {width, height} = Dimensions.get('window');
-const {Value, event, block, cond, eq, set} = Animated;
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  startClock,
+  stopClock,
+  debug,
+  timing,
+  clockRunning,
+  interpolate,
+  Extrapolate,
+  concat,
+} = Animated;
+
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0),
+  };
+
+  const config = {
+    duration: 1000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease),
+  };
+
+  return block([
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug('stop clock', stopClock(clock))),
+    state.position,
+  ]);
+}
 
 class LoginScreen extends Component {
   static navigationOptions = {
@@ -27,9 +70,62 @@ class LoginScreen extends Component {
     this.onStateChange = event([
       {
         nativeEvent: ({state}) =>
-          block([cond(eq(state, State.END), set(this.buttonOpacity,0))]),
+          block([
+            cond(
+              eq(state, State.END),
+              set(this.buttonOpacity, runTiming(new Clock(), 1, 0)),
+            ),
+          ]),
       },
     ]);
+
+    this.onCloseState = event([
+      {
+        nativeEvent: ({state}) =>
+          block([
+            cond(
+              eq(state, State.END),
+              set(this.buttonOpacity, runTiming(new Clock(), 0, 1)),
+            ),
+          ]),
+      },
+    ]);
+
+    this.buttonY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [100, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    this.backGroundY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [-height / 3, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    this.textInputZindex = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [-1, 1],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    this.textInputY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [0, 100],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    this.textInputOpacity = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    this.rotateCross = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [180, 360],
+      extrapolate: Extrapolate.CLAMP,
+    });
   }
   state = {
     email: '',
@@ -91,24 +187,77 @@ class LoginScreen extends Component {
       // </View>
       <View
         style={{flex: 1, backgroundColor: 'white', justifyContent: 'flex-end'}}>
-        <View style={{...StyleSheet.absoluteFill}}>
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFill,
+            transform: [{translateY: this.backGroundY}],
+          }}>
           <Image
             source={require('../src/login.png')}
             style={{flex: 1, height: null, width: null}}></Image>
-        </View>
+        </Animated.View>
+        <View style={{height: height / 3, justifyContent: 'center'}}>
+          <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+            <Animated.View
+              style={{
+                ...styles.button,
+                opacity: this.buttonOpacity,
+                transform: [{translateY: this.buttonY}],
+              }}>
+              <Text style={{fontSize: 20, fontWeight: 'bold'}}>로그인</Text>
+            </Animated.View>
+          </TapGestureHandler>
 
-        <View style={{height: height / 3, backgroundColor: 'white'}}>
-          <View style={styles.button}>
-            <TopGestureHandler onHandlerStateChange={this.onStateChange}>
-              <Animated.View
-                style={{...styles.button, opacity: this.buttonOpacity}}>
-                <Text style={{fontSize: 20, fontWeight: 'bold'}}>로그인</Text>
+          <Animated.View
+            style={{
+              opacity: this.buttonOpacity,
+              transform: [{translateY: this.buttonY}],
+            }}>
+            <View style={styles.button}>
+              <Text style={{fontSize: 20, fontWeight: 'bold'}}>회원가입</Text>
+            </View>
+          </Animated.View>
+
+          <Animated.View
+            style={{
+              zIndex: this.textInputZindex,
+              opacity: this.textInputOpacity,
+              transform: [{translateY: this.textInputY}],
+              height: height / 3,
+              ...StyleSheet.absoluteFill,
+              top: null,
+              justifyContent: 'center',
+            }}>
+            <TapGestureHandler onHandlerStateChange={this.onCloseState}>
+              <Animated.View style={styles.closeButton}>
+                <Animated.Text
+                  style={{
+                    fontSize: 15,
+                    transform: [{rotate: concat(this.rotateCross, 'deg')}],
+                  }}>
+                  X
+                </Animated.Text>
               </Animated.View>
-            </TopGestureHandler>
-          </View>
-          <View style={styles.button}>
-            <Text style={{fontSize: 20, fontWeight: 'bold'}}>회원가입</Text>
-          </View>
+            </TapGestureHandler>
+
+            <TextInput
+              style={styles.textInput}
+              placeholder="EMAIL"
+              autoCapitalize="none"
+              onChangeText={(email) => this.setState({email})}
+              value={this.state.email}></TextInput>
+
+            <TextInput
+              style={styles.textInput}
+              placeholder="PASSWORD"
+              secureTextEntry={true}
+              onChangeText={(password) => this.setState({password})}
+              value={this.state.password}></TextInput>
+
+            <Animated.View style={styles.button}>
+              <Text style={{fontSize: 20, fontWeight: 'bold'}}>로그인</Text>
+            </Animated.View>
+          </Animated.View>
         </View>
       </View>
     );
@@ -146,20 +295,37 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textTransform: 'uppercase',
   },
-  input: {
-    borderBottomColor: '#7f8c8d',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    fontSize: 15,
+  closeButton: {
+    zIndex:1,
     height: 40,
+    width: 40,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -20,
+    left: width / 2 - 20,
+    elevation: 3,
   },
   button: {
-    marginTop: 10,
     marginHorizontal: 25,
-    justifyContent: 'center',
+    marginVertical: 5,
     alignItems: 'center',
-    backgroundColor: '#7f8c8d',
+    justifyContent: 'center',
+    backgroundColor: 'white',
     borderRadius: 35,
     height: 70,
+    elevation: 3,
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 0.5,
+    marginHorizontal: 25,
+    paddingLeft: 10,
+    marginVertical: 5,
+    borderColor: 'black',
   },
 });
 
