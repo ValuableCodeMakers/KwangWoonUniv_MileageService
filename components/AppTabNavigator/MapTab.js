1dbbc88d57fe4c8567122d2fdeceee161df5204d
jiwonkim97 <jiwonkim97@naver.com>
2020-12-27 19:56:54 +0900

    201227_MapTab
    MapTab에서 Google Map API 연동

--- components/AppTabNavigator/MapTab.js
+++ components/AppTabNavigator/MapTab.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
 import { Icon } from 'native-base';
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-import { 
+import {
     View,
     Text,
     StyleSheet
-    } from 'react-native';
- 
-class MapTab extends Component{
+} from 'react-native';
+
+class MapTab extends Component {
     static navigationOptions = {
         tabBarIcon: ({ tintColor }) => (
             <Icon name='ios-map' style={{ color: tintColor }} />
         )
     }
-    render(){
+    render() {
         return (
-            <View style={style.container}>
-                <Text>지도</Text>
+            <View style={{ flex: 1 }}>
+                <MapView
+                    style={{ flex: 1 }}
+                    provider={PROVIDER_GOOGLE}
+                    initialRegion={{
+                        latitude: 37.61947,
+                        longitude: 127.05899,
+                        latitudeDelta: 0.004,
+                        longitudeDelta: 0.002,
+                    }}
+                />
             </View>
         );
     }
 }
 export default MapTab;
- 
+
 const style = StyleSheet.create({
     container: {
         flex: 1,
