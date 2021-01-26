20c9207f3d04c569b89bce516bb1d1fed7f60bd5
jiwonkim97 <jiwonkim97@naver.com>
2021-01-26 17:30:49 +0900

    feat + refactor: 학교건물 좌표 추가 및 좌표들 리팩토링

---
+++ components/AppTabNavigator/Coordinates/Coordinate.js
@@ -0,0 +1,296 @@
+export const KW_Area =
+    [
+        [
+            // 참빛관 시작
+            { latitude: 37.618721, longitude: 127.061062 },
+            { latitude: 37.61894, longitude: 127.061424 },
+            { latitude: 37.620114, longitude: 127.061139 },
+            { latitude: 37.620425, longitude: 127.060679 },
+            { latitude: 37.620382, longitude: 127.060467 },
+            { latitude: 37.620382, longitude: 127.06005 },
+            { latitude: 37.620618, longitude: 127.059637 },
+            { latitude: 37.620618, longitude: 127.059615 },
+            { latitude: 37.620619, longitude: 127.05957 },
+            { latitude: 37.620626, longitude: 127.059521 },
+            { latitude: 37.620678, longitude: 127.059383 },
+            { latitude: 37.62083, longitude: 127.059116 },
+            { latitude: 37.62086, longitude: 127.059048 },
+            { latitude: 37.619225, longitude: 127.05797 },
+            { latitude: 37.619175, longitude: 127.058019 },
+            { latitude: 37.619124, longitude: 127.058176 },
+            { latitude: 37.618957, longitude: 127.058412 },
+            { latitude: 37.618686, longitude: 127.058648 },
+            { latitude: 37.618551, longitude: 127.058875 },
+            { latitude: 37.6191, longitude: 127.05984 },
+            { latitude: 37.6191, longitude: 127.05988 },
+            { latitude: 37.61898, longitude: 127.060064 },
+            { latitude: 37.61907, longitude: 127.060177 },
+            { latitude: 37.619034, longitude: 127.060224 },
+            { latitude: 37.618989, longitude: 127.060714 },
+            { latitude: 37.618721, longitude: 127.061062 },
+        ],
+        [
+            // 연구관 아래 시작
+            { latitude: 37.619164, longitude: 127.057809 },
+            { latitude: 37.619937, longitude: 127.05829 },
+            { latitude: 37.620139, longitude: 127.05749 },
+            { latitude: 37.620176, longitude: 127.057416 },
+            { latitude: 37.620396, longitude: 127.057286 },
+            { latitude: 37.620623, longitude: 127.057416 },
+            { latitude: 37.620974, longitude: 127.057273 },
+            { latitude: 37.620996, longitude: 127.057195 },
+            { latitude: 37.620984, longitude: 127.057069 },
+            { latitude: 37.621524, longitude: 127.056722 },
+            { latitude: 37.621657, longitude: 127.05656 },
+            { latitude: 37.621732, longitude: 127.055994 },
+            { latitude: 37.621632, longitude: 127.055176 },
+            { latitude: 37.621166, longitude: 127.054638 },
+            { latitude: 37.620735, longitude: 127.054395 },
+            { latitude: 37.620637, longitude: 127.054478 },
+            { latitude: 37.620497, longitude: 127.054541 },
+            { latitude: 37.620142, longitude: 127.054637 },
+            { latitude: 37.619719, longitude: 127.055348 },
+            { latitude: 37.619768, longitude: 127.05545 },
+            { latitude: 37.619431, longitude: 127.056207 },
+            { latitude: 37.619236, longitude: 127.056366 },
+            { latitude: 37.619214, longitude: 127.05646 },
+            { latitude: 37.619343, longitude: 127.056502 },
+            { latitude: 37.619475, longitude: 127.056477 },
+            { latitude: 37.619693, longitude: 127.056777 },
+            { latitude: 37.619698, longitude: 127.056826 },
+            { latitude: 37.619779, longitude: 127.05694 },
+            { latitude: 37.619766, longitude: 127.056954 },
+            { latitude: 37.619776, longitude: 127.056974 },
+            { latitude: 37.619164, longitude: 127.057809 },
+        ]
+    ];
+
+export const HwaDo = {
+    point: { latitude: 37.6204659, longitude: 127.05944285 },
+    coordinate: [
+        { latitude: 37.620704, longitude: 127.059090 },
+        { latitude: 37.620686, longitude: 127.059071 },
+        { latitude: 37.620682, longitude: 127.059079 },
+        { latitude: 37.620591, longitude: 127.059024 },
+        { latitude: 37.620566, longitude: 127.059081 },
+        { latitude: 37.620520, longitude: 127.059054 },
+        { latitude: 37.620431, longitude: 127.059260 },
+        { latitude: 37.620402, longitude: 127.059246 },
+        { latitude: 37.620269, longitude: 127.059559 },
+        { latitude: 37.620296, longitude: 127.059573 },
+        { latitude: 37.620204, longitude: 127.059792 },
+        { latitude: 37.620225, longitude: 127.059897 },
+        { latitude: 37.620309, longitude: 127.059945 },
+        { latitude: 37.620300, longitude: 127.059969 },
+        { latitude: 37.620334, longitude: 127.059991 },
+        { latitude: 37.620389, longitude: 127.059872 },
+        { latitude: 37.620372, longitude: 127.059853 },
+        { latitude: 37.620657, longitude: 127.059199 },
+        { latitude: 37.620677, longitude: 127.059212 },
+        { latitude: 37.620704, longitude: 127.059090 },
+    ],
+    title: '화도관'
+};
+
+export const BiMa = {
+    point: { latitude: 37.619515535848116, longitude: 127.05991798998568, },
+    coordinate: [
+        { latitude: 37.619143, longitude: 127.059720 },
+        { latitude: 37.619214, longitude: 127.059943 },
+        { latitude: 37.619220, longitude: 127.059939 },
+        { latitude: 37.619281, longitude: 127.060146 },
+        { latitude: 37.619268, longitude: 127.060151 },
+        { latitude: 37.619296, longitude: 127.060240 },
+        { latitude: 37.619334, longitude: 127.060223 },
+        { latitude: 37.619391, longitude: 127.060408 },
+        { latitude: 37.619434, longitude: 127.060388 },
+        { latitude: 37.619445, longitude: 127.060425 },
+        { latitude: 37.619725, longitude: 127.060272 },
+        { latitude: 37.619736, longitude: 127.060285 },
+        { latitude: 37.619765, longitude: 127.060267 },
+        { latitude: 37.619768, longitude: 127.060253 },
+        { latitude: 37.619750, longitude: 127.060186 },
+        { latitude: 37.619868, longitude: 127.059853 },
+        { latitude: 37.619761, longitude: 127.059539 },
+        { latitude: 37.619701, longitude: 127.059565 },
+        { latitude: 37.619692, longitude: 127.059536 },
+        { latitude: 37.619682, longitude: 127.059539 },
+        { latitude: 37.619662, longitude: 127.059479 },
+        { latitude: 37.619656, longitude: 127.059480 },
+        { latitude: 37.619645, longitude: 127.059448 },
+        { latitude: 37.619143, longitude: 127.059720 },
+    ],
+    title: '비마관'
+};
+
+export const OgUi = {
+    point: { latitude: 37.618791, longitude: 127.059079 },
+    coordinate: [
+        { latitude: 37.618615, longitude: 127.058872 },
+        { latitude: 37.618880, longitude: 127.059399 },
+        { latitude: 37.618957, longitude: 127.059336 },
+        { latitude: 37.618965, longitude: 127.059352 },
+        { latitude: 37.618988, longitude: 127.059336 },
+        { latitude: 37.618786, longitude: 127.058926 },
+        { latitude: 37.618777, longitude: 127.058835 },
+        { latitude: 37.618790, longitude: 127.058821 },
+        { latitude: 37.618782, longitude: 127.058802 },
+        { latitude: 37.618779, longitude: 127.058780 },
+        { latitude: 37.618760, longitude: 127.058758 },
+        { latitude: 37.618615, longitude: 127.058872 },
+    ],
+    title: '옥의관'
+};
+
+export const BokJi = {
+    point: { latitude: 37.619226, longitude: 127.058251 },
+    coordinate: [
+        { latitude: 37.618956, longitude: 127.058529 },
+        { latitude: 37.619046, longitude: 127.058608 },
+        { latitude: 37.61918, longitude: 127.05835 },
+        { latitude: 37.619322, longitude: 127.058312 },
+        { latitude: 37.619607, longitude: 127.058487 },
+        { latitude: 37.619662, longitude: 127.058362 },
+        { latitude: 37.619300, longitude: 127.058137 },
+        { latitude: 37.619143, longitude: 127.058168 },
+        { latitude: 37.618956, longitude: 127.058529 },
+    ],
+    title: '복지관'
+};
+
+export const YeonGu = {
+    point: { latitude: 37.61947250274096, longitude: 127.05763228716857 },
+    coordinate: [
+        { latitude: 37.61931106380559, longitude: 127.05773905454224 },
+        { latitude: 37.61944701937686, longitude: 127.05788319675851 },
+        { latitude: 37.619517626039574, longitude: 127.05790785266392 },
+        { latitude: 37.61950485675448, longitude: 127.05778267652875 },
+        { latitude: 37.61952438624844, longitude: 127.05771155372466 },
+        { latitude: 37.619615273441326, longitude: 127.05776560705576 },
+        { latitude: 37.619649825568146, longitude: 127.0573085245016 },
+        { latitude: 37.619601753039504, longitude: 127.05730473128538 },
+        { latitude: 37.61931106380559, longitude: 127.05773905454224 },
+    ],
+    title: '연구관'
+};
+
+export const DongHae = {
+    point: { latitude: 37.61985491716964, longitude: 127.05758668961711 },
+    coordinate: [
+        { latitude: 37.619597997378015, longitude: 127.05786328237406 },
+        { latitude: 37.61965959030286, longitude: 127.05792871535378 },
+        { latitude: 37.61974822393443, longitude: 127.05799414833353 },
+        { latitude: 37.61992398862008, longitude: 127.05808992704303 },
+        { latitude: 37.620023888877775, longitude: 127.05764801735165 },
+        { latitude: 37.619981074498874, longitude: 127.0576347410949 },
+        { latitude: 37.62010876643441, longitude: 127.05716248566017 },
+        { latitude: 37.619811319000696, longitude: 127.05704584426148 },
+        { latitude: 37.619751228463734, longitude: 127.0572117974737 },
+        { latitude: 37.61967235956127, longitude: 127.05719283139595 },
+        { latitude: 37.61964516159121, longitude: 127.05730609536565 },
+        { latitude: 37.619597997378015, longitude: 127.05786328237406 },
+    ],
+    title: '동해문화예술관'
+};
+
+export const ChamBit = {
+    point: { latitude: 37.619238, longitude: 127.060908 },
+    coordinate: [
+        { latitude: 37.618956420432404, longitude: 127.06092732179584 },
+        { latitude: 37.61910726296122, longitude: 127.06090787578123 },
+        { latitude: 37.61913222630794, longitude: 127.06099840033193 },
+        { latitude: 37.61893411270854, longitude: 127.06109160709318 },
+        { latitude: 37.61893889293564, longitude: 127.06111440586892 },
+        { latitude: 37.61892508338979, longitude: 127.06112178194344 },
+        { latitude: 37.61894420429873, longitude: 127.06120828318076 },
+        { latitude: 37.61898032155776, longitude: 127.06120090710624 },
+        { latitude: 37.61898403950987, longitude: 127.06120425986738 },
+        { latitude: 37.61926076085425, longitude: 127.0610795371531 },
+        { latitude: 37.6192517315752, longitude: 127.06103930401945 },
+        { latitude: 37.619310156303726, longitude: 127.06100778806336 },
+        { latitude: 37.6193000647632, longitude: 127.060974260452 },
+        { latitude: 37.619309094036375, longitude: 127.06096621382527 },
+        { latitude: 37.61926182312346, longitude: 127.06079723466395 },
+        { latitude: 37.61925066930815, longitude: 127.06079857576843 },
+        { latitude: 37.6192427022962, longitude: 127.06076773036595 },
+        { latitude: 37.619178966169855, longitude: 127.06078717638056 },
+        { latitude: 37.61917365482353, longitude: 127.0607663892615 },
+        { latitude: 37.618938886857826, longitude: 127.06081134266417 },
+        { latitude: 37.618956420432404, longitude: 127.06092732179584 },
+    ],
+    title: '참빛관'
+};
+
+export const SaeBit = {
+    point: { latitude: 37.61973223747628, longitude: 127.06088358524107 },
+    coordinate: [
+        { latitude: 37.619551337778326, longitude: 127.06098443004952 },
+        { latitude: 37.619830181531114, longitude: 127.0610762957047 },
+        { latitude: 37.619901352911896, longitude: 127.06078326438129 },
+        { latitude: 37.61961294905942, longitude: 127.06069139872614 },
+        { latitude: 37.619551337778326, longitude: 127.06098443004952 },
+    ],
+    title: '새빛관'
+};
+
+export const HanWool = {
+    point: { latitude: 37.62065002559912, longitude: 127.05701583976301 },
+    coordinate: [
+        { latitude: 37.6203583497507, longitude: 127.05700308247482 },
+        { latitude: 37.62039590603417, longitude: 127.05718515685325 },
+        { latitude: 37.620759449877426, longitude: 127.05707136036675 },
+        { latitude: 37.6208015126867, longitude: 127.05729041860329 },
+        { latitude: 37.62096976368578, longitude: 127.05723446866408 },
+        { latitude: 37.6208878915954, longitude: 127.05683523265722 },
+        { latitude: 37.6203583497507, longitude: 127.05700308247482 },
+    ],
+    title: '한울관'
+};
+
+export const NooRi = {
+    point: { latitude: 37.62022384033076, longitude: 127.05472153533972 },
+    coordinate: [
+        { latitude: 37.62004941660347, longitude: 127.05500857566201 },
+        { latitude: 37.62050406111984, longitude: 127.05531971189554 },
+        { latitude: 37.62056779611022, longitude: 127.05522851679262 },
+        { latitude: 37.6205401776211, longitude: 127.05520705912134 },
+        { latitude: 37.62058691659668, longitude: 127.0550863597204 },
+        { latitude: 37.62063578003973, longitude: 127.05513195727187 },
+        { latitude: 37.62069101693667, longitude: 127.05501930449766 },
+        { latitude: 37.62022787553015, longitude: 127.05469743942851 },
+        { latitude: 37.620187509858035, longitude: 127.0547805879047 },
+        { latitude: 37.6201365216093, longitude: 127.05478327011362 },
+        { latitude: 37.620094031375295, longitude: 127.05485568975416 },
+        { latitude: 37.62009190686296, longitude: 127.0548637363809 },
+        { latitude: 37.62004941660347, longitude: 127.05500857566201 },
+    ],
+    title: '누리관'
+};
+
+export const Anni80 = {
+    point: { latitude: 37.61992359803878, longitude: 127.05869847544469 },
+    coordinate: [
+        { latitude: 37.6199110449105, longitude: 127.05858637740856 },
+        { latitude: 37.61986051506595, longitude: 127.05872831914397 },
+        { latitude: 37.62039233995771, longitude: 127.059029745977848 },
+        { latitude: 37.62043276354694, longitude: 127.05889418364629 },
+        { latitude: 37.6199110449105, longitude: 127.05858637740856 },
+    ],
+    title: '80주년기념관'
+};
+
+export const IceLink = {
+    point: { latitude: 37.620288442143405, longitude: 127.05719002525126 },
+    coordinate: [
+        { latitude: 37.620190276417595, longitude: 127.05727750975406 },
+        { latitude: 37.62023276659658, longitude: 127.05728890914193 },
+        { latitude: 37.62035970586167, longitude: 127.05721112508357 },
+        { latitude: 37.620325182652046, longitude: 127.0570729913247 },
+        { latitude: 37.620214177146266, longitude: 127.05714138765191 },
+        { latitude: 37.620215770527906, longitude: 127.05717223305436 },
+        { latitude: 37.62020461685572, longitude: 127.05718698520334 },
+        { latitude: 37.62020355460115, longitude: 127.05722319502365 },
+        { latitude: 37.620190276417595, longitude: 127.05727750975406 },
+    ],
+    title: '아이스링크'
+};
